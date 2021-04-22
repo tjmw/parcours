ffb12b887d920e419f6622839950ac4f49a9ab08
Tom Wey <tjmwey@gmail.com>
2021-04-22 11:03:33 +0100

    Animate the cube

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,8 @@ document.body.appendChild(renderer.domElement);
 
 const animate = () => {
   requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 };
 
