52c6da2a056d5c427df6b5618e680f35c3d2c43e
Tom Wey <tjmwey@gmail.com>
2021-04-23 09:29:17 +0100

    Color code lines based on elevation

--- src/renderRoutePoints.ts
+++ src/renderRoutePoints.ts
@@ -4,6 +4,13 @@ import { Point } from './types';
 
 const SHOULD_ROTATE = true;
 
+const COLOR_MAPPINGS = [0xc70039, 0xff5733, 0xffc300, 0x64d2ac, 0xdaf7a6];
+
+const colorFromEle = (ele: number, minEle: number, maxEle: number) => {
+	const normalizedEle = THREE.MathUtils.mapLinear(ele, minEle, maxEle, 0, 5);
+	return COLOR_MAPPINGS[Math.floor(normalizedEle)];
+};
+
 export const renderRoutePoints = (routePoints: Point[]) => {
 	const { minLon, maxLon, minLat, maxLat, minEle, maxEle } = getRouteBounds(
 		routePoints,
@@ -32,24 +39,58 @@ export const renderRoutePoints = (routePoints: Point[]) => {
 	// |  /
 	// |_______________X (lon - E/W)
 
-	const material = new THREE.LineBasicMaterial({ color: 0xf0f3f4 });
-	const points = routePoints.map((rp: Point) => {
-		const x = THREE.MathUtils.mapLinear(rp.lon, minLon, maxLon, 0, 100);
-		const y = THREE.MathUtils.mapLinear(rp.lat, minLat, maxLat, 0, 150);
-		const z = THREE.MathUtils.mapLinear(rp.ele, minEle, maxEle, 0, 20);
-		return new THREE.Vector3(x, y, z);
+	const map = new THREE.Group();
+
+	routePoints.forEach((rp, idx) => {
+		const nextRp = routePoints[idx + 1];
+
+		if (nextRp) {
+			const lineColor = colorFromEle(rp.ele, minEle, maxEle);
+			const material = new THREE.LineBasicMaterial({ color: lineColor });
+
+			// Start point
+			const x = THREE.MathUtils.mapLinear(rp.lon, minLon, maxLon, 0, 100);
+			const y = THREE.MathUtils.mapLinear(rp.lat, minLat, maxLat, 0, 150);
+			const z = THREE.MathUtils.mapLinear(rp.ele, minEle, maxEle, 0, 20);
+			const a = new THREE.Vector3(x, y, z);
+
+			// End point
+			const x1 = THREE.MathUtils.mapLinear(
+				nextRp.lon,
+				minLon,
+				maxLon,
+				0,
+				100,
+			);
+			const y1 = THREE.MathUtils.mapLinear(
+				nextRp.lat,
+				minLat,
+				maxLat,
+				0,
+				150,
+			);
+			const z1 = THREE.MathUtils.mapLinear(
+				nextRp.ele,
+				minEle,
+				maxEle,
+				0,
+				20,
+			);
+			const b = new THREE.Vector3(x1, y1, z1);
+
+			const geometry = new THREE.BufferGeometry().setFromPoints([a, b]);
+			const line = new THREE.Line(geometry, material);
+			map.add(line);
+		}
 	});
 
-	const geometry = new THREE.BufferGeometry().setFromPoints(points);
-	const line = new THREE.Line(geometry, material);
-	scene.add(line);
-
+	scene.add(map);
 
 	const animate = () => {
 		requestAnimationFrame(animate);
 
 		if (SHOULD_ROTATE) {
-			line.rotation.y += 0.01;
+			map.rotation.y += 0.01;
 		}
 
 		renderer.render(scene, camera);
