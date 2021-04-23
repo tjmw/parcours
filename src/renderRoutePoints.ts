1da87f289863beb4743a4b9096f027c1de8a25ec
Tom Wey <tjmwey@gmail.com>
2021-04-23 09:29:04 +0100

    Remove old commented cube code

--- src/renderRoutePoints.ts
+++ src/renderRoutePoints.ts
@@ -24,12 +24,6 @@ export const renderRoutePoints = (routePoints: Point[]) => {
 
 	const scene = new THREE.Scene();
 
-	// const g = new THREE.BoxGeometry();
-	// const m = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-	// const cube = new THREE.Mesh(g, m);
-	// cube.position.set(-1, 1, -10);
-	// scene.add(cube);
-
 	// Y (lat - N/S) Z (ele)
 	// |          /
 	// |        /
@@ -50,24 +44,6 @@ export const renderRoutePoints = (routePoints: Point[]) => {
 	const line = new THREE.Line(geometry, material);
 	scene.add(line);
 
-	// const scene = new THREE.Scene();
-	// const camera = new THREE.PerspectiveCamera(
-	// 	75,
-	// 	window.innerWidth / window.innerHeight,
-	// 	0.1,
-	// 	1000,
-	// );
-
-	// const geometry = new THREE.BoxGeometry();
-	// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-	// const cube = new THREE.Mesh(geometry, material);
-	// scene.add(cube);
-
-	// camera.position.z = 5;
-
-	// const renderer = new THREE.WebGLRenderer();
-	// renderer.setSize(window.innerWidth, window.innerHeight);
-	// document.body.appendChild(renderer.domElement);
 
 	const animate = () => {
 		requestAnimationFrame(animate);
